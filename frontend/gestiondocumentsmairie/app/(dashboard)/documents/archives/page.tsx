b09150063942b93c1cfdfb7d44d73b4c6d31f285
islambodi <islamourobodi0@gmail.com>
2025-09-02 22:41:19 +0000

    accès restreint sur les archives document utilisateur paramètre mairie

--- frontend/gestiondocumentsmairie/app/(dashboard)/documents/archives/page.tsx
+++ frontend/gestiondocumentsmairie/app/(dashboard)/documents/archives/page.tsx
@@ -1,6 +1,40 @@
-import DocumentsArchivesPage from "@/components/documents/archives"
+"use client"
 
-export default function documentsArchivesPage() {
+import DocumentsArchivesPage from "@/components/documents/archives"
+import { getCurrentUser } from "@/services/userService"
+import  {Role}  from "@/types/LoginResponse" // adapte le chemin selon ton projet
+import { useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
+
+export default function ArchivedDocumentsPageWrapper() {
+  const router = useRouter()
+  const [authorized, setAuthorized] = useState(false)
+  const [loading, setLoading] = useState(true)
+
+   useEffect(() => {
+    // console.log("useEffect lancé") 
+    async function checkRole() {
+      try {
+        const user = await getCurrentUser()
+        // console.log("Role utilisateur :", user.role) // Ajoute ce log
+        if (user.role !== Role.ADMIN) {
+          router.replace("/403")
+        } else {
+          setAuthorized(true)
+        }
+      } catch {
+        router.replace("/403")
+      } finally {
+        setLoading(false)
+      }
+    }
+    checkRole()
+  }, [router])
+
+  if (!authorized) return null
+
+  if (!authorized) return null
+
   return (
     <div>
       <DocumentsArchivesPage />
--- frontend/gestiondocumentsmairie/app/(dashboard)/utilisateurs/mairies/page.tsx
+++ frontend/gestiondocumentsmairie/app/(dashboard)/utilisateurs/mairies/page.tsx
@@ -1,6 +1,39 @@
+"use client"
+
 import MairieDetails from "@/components/utilisateurs/MairieSettings";
+import { getCurrentUser } from "@/services/userService"
+import  {Role}  from "@/types/LoginResponse" // adapte le chemin selon ton projet
+import { useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
 
 export default function MairiePage() {
+  const router = useRouter()
+  const [authorized, setAuthorized] = useState(false)
+  const [loading, setLoading] = useState(true)
+
+   useEffect(() => {
+    // console.log("useEffect lancé") 
+    async function checkRole() {
+      try {
+        const user = await getCurrentUser()
+        // console.log("Role utilisateur :", user.role) // Ajoute ce log
+        if (user.role !== Role.ADMIN) {
+          router.replace("/403")
+        } else {
+          setAuthorized(true)
+        }
+      } catch {
+        router.replace("/403")
+      } finally {
+        setLoading(false)
+      }
+    }
+    checkRole()
+  }, [router])
+
+  if (!authorized) return null
+
+  if (!authorized) return null
   return (
     <div className="container mx-auto">
       <MairieDetails />
